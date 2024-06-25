ec1eaa44dabda93b5288007a95a83e02e7b86aa7
Sohailshaikh5656 <134220674+Sohailshaikh5656@users.noreply.github.com>
2024-06-25 15:07:25 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Activate virtual environment using vercel python
+vercel python -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
 # Install Python packages from requirements.txt
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run Django collectstatic command
-python3.9 manage.py collectstatic
+python manage.py collectstatic
